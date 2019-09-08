e72e075ea5c9fc928b206d217caff8b939827c5b
followedwind <followedwind@gmail.com>
2019-09-08 20:49:20 +0900

    アクセス方法変更

--- home-state/main.go
+++ home-state/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	//"fmt"
+    //"bytes"
+    //"net/url"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
     "github.com/followedwind/home-state-lambda/home-state/util"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    buffer, err := util.S3Get("config/ip.txt")
+
+    command := "connection_check"
+    //param := url.Values{}
+    //param.Set("command", command)
+    param := make(map[string]interface{})
+    param["command"] = command
+    res, err := util.HomeAccess(command, param)
+
 	return events.APIGatewayProxyResponse{
-		Body:       string(buffer.Bytes()),
+		Body:       string(res),
 		StatusCode: 200,
 	}, err
 }
---
+++ home-state/util/homeaccess.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+    //"net/url"
+    "net/http"
+    "io/ioutil"
+    "encoding/json"
+)
+
+//func HomeAccess (subdir string, param url.Values) ([]byte, error) {
+func HomeAccess (subdir string, param map[string]interface{}) ([]byte, error) {
+
+    buf, err := S3Get("config/endpoint.txt")
+    str := string(buf.Bytes())
+    jsonParam, _ := json.Marshal(param)
+    req, _ := http.NewRequest("POST", str, bytes.NewBuffer(jsonParam))
+
+    //以下適当
+    //param挟む
+    client := &http.Client{}
+    resp, _ := client.Do(req)
+    fmt.Println(str)
+    fmt.Println(param["command"])
+    defer resp.Body.Close()
+
+    byteArray, _ := ioutil.ReadAll(resp.Body)
+    return byteArray, err
+}
